d6935e5fc37ea34729040a6da2be8c8a1b197f94
Abhishekkvinfo <abishekkvinfo@gmail.com>
2024-08-05 20:27:19 +0530

    values-and-variables

--- basics _01/values-and-variables.js
+++ basics _01/values-and-variables.js
@@ -5,3 +5,9 @@ document.write(person)
 let first_name = "jonas"
 
 console.log(first_name)
+
+let myfirstjob = "programmer";
+let mycurrentjob ="teacher";
+
+console.log(mycurrentjob)
+console.log(myfirstjob)
