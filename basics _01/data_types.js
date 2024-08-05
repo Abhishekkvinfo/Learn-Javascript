d613e1f6b3e4a4a9fa1788284729ca4c9b7fed1d
Abhishekkvinfo <abishekkvinfo@gmail.com>
2024-08-05 21:25:28 +0530

    data_types

---
+++ basics _01/data_types.js
@@ -0,0 +1,13 @@
+let isIsland = true;
+
+let country = true;
+let language ;
+
+console.log(typeof isIsland)
+console.log(typeof country)
+console.log(typeof language)
+
+
+console.log(typeof "name")
+console.log(typeof 23.067845)
+console.log(typeof 23)
