78afa5c45bca2f5ad9aadac32aef0061b5985ede
Abhishekkvinfo <abishekkvinfo@gmail.com>
2024-08-06 22:44:27 +0530

    data_types

--- basics _01/data_types.js
+++ basics _01/data_types.js
@@ -9,3 +9,7 @@ console.log(typeof language)
 console.log(typeof "name")
 console.log(typeof 23.067845)
 console.log(typeof 23)
+console.log(typeof "23")
+console.log(typeof null)
+
+document.write(typeof language)
