c808b46be8d39f24da03939fbbae63096a6c8b2e
Abhishekkvinfo <abishekkvinfo@gmail.com>
2024-08-07 22:41:34 +0530

    assignment operators

--- basics _01/Basic Operators.js
+++ basics _01/Basic Operators.js
@@ -1,9 +1,9 @@
 
+//math operators
 
 const now = "2024";
 const ageofabhi = now - 2007;
 const ageofsarah = now -2004;
-
 
 console.log(ageofabhi,ageofsarah)
 console.log(ageofabhi *2,ageofabhi /2 , 2**3);
@@ -11,5 +11,14 @@ console.log(ageofabhi *2,ageofabhi /2 , 2**3);
 const firstname = "Abhishek";
 const lastname = "kalady";
 
-
 console.log(firstname + lastname);
+
+//assignment operators
+
+let x =10+5;
+x += 10;
+x++;
+x--;
+x--;
+
+console.log(x)
