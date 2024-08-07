d90931a980b14a6cdef67c61ef6b4627a31073c1
Abhishekkvinfo <abishekkvinfo@gmail.com>
2024-08-07 22:20:23 +0530

    009 Basic Operators

--- basics _01/009 Basic Operators.js
+++ basics _01/009 Basic Operators.js
@@ -4,4 +4,6 @@ const now = "2024";
 const ageofabhi = now - 2007;
 const ageofsarah = now -2004;
 
-console,console.log(ageofabhi,ageofsarah);
+
+console.log(ageofabhi,ageofsarah)
+console.log(ageofabhi *2,ageofabhi /2 , 2**3);
