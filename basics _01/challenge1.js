94853dfa097147ef51d20e948dbf32ca0f9644c1
Abhishekkvinfo <abishekkvinfo@gmail.com>
2024-08-09 23:00:23 +0530

    challenge

--- basics _01/challenge1.js
+++ basics _01/challenge1.js
@@ -8,3 +8,7 @@ const BMIMARK = massMark / heightMark **2;
 const BMIJOHN = massJohn / heightJohn **2;
 
 console.log(BMIMARK,BMIJOHN)
+
+const markhigherBMI = BMIMARK > BMIJOHN;
+
+console.log(BMIMARK,BMIJOHN, markhigherBMI)
