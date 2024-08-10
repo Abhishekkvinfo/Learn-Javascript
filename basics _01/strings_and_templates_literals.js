4e918cbe656600416d1e7a298420d9b2f475b13b
Abhishekkvinfo <abishekkvinfo@gmail.com>
2024-08-10 21:19:54 +0530

    strings_and_templates_literals

--- basics _01/strings_and_templates_literals.js
+++ basics _01/strings_and_templates_literals.js
@@ -7,3 +7,6 @@ const year = "2024"
 const jonas = " I,m " + firstname  + " a " + (year - birthyear)   +" old " + job;
 
 console.log(jonas)
+
+const jonasnew =`i,m ${firstname} a ${year - birthyear} old ${job}`;
+console.log(jonasnew)
