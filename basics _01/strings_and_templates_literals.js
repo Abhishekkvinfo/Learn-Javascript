9a0a4d82e313993382ab277b1d9760e7fd939745
Abhishekkvinfo <abishekkvinfo@gmail.com>
2024-08-10 20:44:51 +0530

    strings_and_templates_literals

---
+++ basics _01/strings_and_templates_literals.js
@@ -0,0 +1,9 @@
+
+const firstname ="jonas";
+const job = "teacher";
+const birthyear = "1991";
+const year = "2024"
+
+const jonas = " I,m " + firstname  + " a " +  birthyear  +" old " + job;
+
+console.log(jonas)
