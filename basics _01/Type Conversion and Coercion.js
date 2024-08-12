b19bf7a7e7da61416472006e7f6146d5245c1be3
Abhishekkvinfo <abishekkvinfo@gmail.com>
2024-08-12 21:26:38 +0530

    Type Conversion and Coercion

--- basics _01/Type Conversion and Coercion.js
+++ basics _01/Type Conversion and Coercion.js
@@ -4,9 +4,9 @@
 const inputyear = "2007";
 
 console.log((Number)(inputyear)+18);
-console.log(typeof inputyear)
+console.log(typeof inputyear);
 
 //type coercion
-console.log("iam " +18+ " years old")
-console.log('30' - '20' +5)
-console.log(12+12+'20')
+console.log("iam " + 18 + " years old");
+console.log('30' - '20' +5);
+console.log(12+12+'20');
