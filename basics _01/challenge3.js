f4ad236edffb935ac1069890ad585e87c8d9f96e
Abhishekkvinfo <abishekkvinfo@gmail.com>
2024-08-13 23:05:06 +0530

    challenge

---
+++ basics _01/challenge3.js
@@ -0,0 +1,22 @@
+
+
+const scoreDolphins =(50+108+89)/3;
+const scoreKoalas =(50+108+89)/3;
+
+console.log(scoreDolphins,scoreKoalas);
+
+if (scoreDolphins>scoreKoalas){
+
+    console.log("Dolphins win the trophy ")
+}
+else if(scoreKoalas>scoreDolphins){
+
+    console.log("Koalas win the trophy ")   
+}
+else if (scoreDolphins === scoreKoalas && scoreDolphins >=95 && scoreKoalas >=95){
+
+    console.log("score of dolphines and koalas are equal")
+}else{
+
+    console.log("both are lost")
+}
