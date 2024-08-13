a2fde56a366a8d2e634c4978b42003976de06ec7
Abhishekkvinfo <abishekkvinfo@gmail.com>
2024-08-13 19:17:53 +0530

    logical-operators

---
+++ basics _01/logical-operators.js
@@ -0,0 +1,27 @@
+
+const abhishekcandrive =true;
+const abhishekhasgoodvision =true;
+
+// AND operator
+
+if(abhishekcandrive && abhishekhasgoodvision){
+
+    console.log("he can drive ")
+}
+
+// 0R operator
+
+const abhicandrive =false;
+const abhihasgoodvision =false;
+
+if (abhicandrive || abhihasgoodvision){
+
+    console.log("he can drive ")
+}else{
+
+    console.log("he can't drive ")
+}
+
+//NOT operator
+
+console.log(! abhihasgoodvision )
