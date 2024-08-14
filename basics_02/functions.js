a945a46de3a8e2441bb80d8cfc1183a8e1013018
Abhishekkvinfo <abishekkvinfo@gmail.com>
2024-08-14 23:58:47 +0530

    function

--- basics_02/functions.js
+++ basics_02/functions.js
@@ -9,6 +9,8 @@ function logger(){
 logger();
 logger();
 
+//function declaration
+
 function fruitprocessor(apples , oranges){
     console.log(apples , oranges);
     const juice = `juice with ${apples} apples and ${oranges} oranges`;
@@ -20,6 +22,7 @@ console.log(apple)
 const orange = fruitprocessor(22,99);
 console.log(orange)
 
+
 function calcage(birthyear){
 
     return 2024-birthyear;
@@ -28,6 +31,8 @@ function calcage(birthyear){
 const age1 = calcage(2007);
 console.log(age1);
 
+
+//function expression
 const age2 = function(birthyear0){
 return 2024-birthyear0;
 
