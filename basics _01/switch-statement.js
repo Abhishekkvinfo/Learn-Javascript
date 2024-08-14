6d961a205f98780550e3b5d1809df8e5d4659625
Abhishekkvinfo <abishekkvinfo@gmail.com>
2024-08-14 11:19:05 +0530

    switch statements

--- basics _01/switch-statement.js
+++ basics _01/switch-statement.js
@@ -1,36 +1,74 @@
  
-const day = "tuesday";
+// const day = "tuesday";
 
-switch(day){
+// switch(day){
 
-    case "monday" :
-    console.log("Working day ")
-    break;
+//     case "monday" :
+//     console.log("Working day ")
+//     break;
     
-    case "tuesday" :
-    console.log("Submit assignment ")
-    break;
+//     case "tuesday" :
+//     console.log("Submit assignment ")
+//     break;
 
-    case "wednesday" :
-    console.log("Complete notes")
-    break;
+//     case "wednesday" :
+//     console.log("Complete notes")
+//     break;
 
-    case "thursday" :
-    console.log("Go for a movie ")
-    break;
+//     case "thursday" :
+//     console.log("Go for a movie ")
+//     break;
 
-    case "friday" :
-    console.log("sleep well")
-    break;
+//     case "friday" :
+//     console.log("sleep well")
+//     break;
 
-    case "saturday" :
-    console.log("enjoy the weekend")
-    break;
+//     case "saturday" :
+//     console.log("enjoy the weekend")
+//     break;
 
-    case "sunday" :
-    console.log("enjoy the weekend ")
-    break;
+//     case "sunday" :
+//     console.log("enjoy the weekend ")
+//     break;
 
-    default:
-        console.log("invalid day")
+//     default:
+//         console.log("invalid day")
+// }
+
+// using if else statement
+
+
+const day1 = 'tuesday';
+
+if(day1 ==='monday'){
+
+    console.log("Working day!!")
+
+}else if(day1 === 'tuesday'){
+
+    console.log("submit assignments")
+}
+else if(day1 === 'wednesday'){
+
+    console.log("go for a ride")
+}
+
+else if(day1 === 'thursday'){
+
+    console.log("you should go for a walk")
+}
+else if(day1 === 'friday'){
+
+    console.log("go for a ride")
+}
+else if(day1 === 'saturday'){
+
+    console.log("Fun time")
+}
+else if(day1 === 'sunday'){
+
+    console.log("go for a ride")
+}else{
+
+    console.log("invalid number")
 }
