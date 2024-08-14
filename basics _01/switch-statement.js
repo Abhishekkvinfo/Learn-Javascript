fe25148db2c8500ac57ff880c7b4cc1af2407e60
Abhishekkvinfo <abishekkvinfo@gmail.com>
2024-08-14 11:20:25 +0530

    switch-statements

--- basics _01/switch-statement.js
+++ basics _01/switch-statement.js
@@ -1,39 +1,39 @@
  
-// const day = "tuesday";
+const day = "tuesday";
 
-// switch(day){
+switch(day){
 
-//     case "monday" :
-//     console.log("Working day ")
-//     break;
+    case "monday" :
+    console.log("Working day ")
+    break;
     
-//     case "tuesday" :
-//     console.log("Submit assignment ")
-//     break;
+    case "tuesday" :
+    console.log("Submit assignment ")
+    break;
 
-//     case "wednesday" :
-//     console.log("Complete notes")
-//     break;
+    case "wednesday" :
+    console.log("Complete notes")
+    break;
 
-//     case "thursday" :
-//     console.log("Go for a movie ")
-//     break;
+    case "thursday" :
+    console.log("Go for a movie ")
+    break;
 
-//     case "friday" :
-//     console.log("sleep well")
-//     break;
+    case "friday" :
+    console.log("sleep well")
+    break;
 
-//     case "saturday" :
-//     console.log("enjoy the weekend")
-//     break;
+    case "saturday" :
+    console.log("enjoy the weekend")
+    break;
 
-//     case "sunday" :
-//     console.log("enjoy the weekend ")
-//     break;
+    case "sunday" :
+    console.log("enjoy the weekend ")
+    break;
 
-//     default:
-//         console.log("invalid day")
-// }
+    default:
+        console.log("invalid day")
+}
 
 // using if else statement
 
