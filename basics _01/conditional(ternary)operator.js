60670b35d7f9be41bc450dd343ae3c728b8ba8c9
Abhishekkvinfo <abishekkvinfo@gmail.com>
2024-08-14 12:05:59 +0530

    conditional(ternary)operator

--- basics _01/conditional(ternary)operator.js
+++ basics _01/conditional(ternary)operator.js
@@ -1,8 +1,9 @@
 
-const  age = 19;
+const  age = 15;
 
 age >=18 ? console.log("drink wine"):console.log("drink water")
 
 const drinkage =age >= 18 ? 'wine': 'water';
 console.log(drinkage)
 
+console.log(`i want ${age >= 18 ? 'wine': 'water'}`)
