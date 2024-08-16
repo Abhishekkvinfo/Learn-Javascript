71105f62b43ea400095a0b05fbddf74279cc313e
Abhishekkvinfo <abishekkvinfo@gmail.com>
2024-08-16 23:45:47 +0530

    function calling other functions

--- basics_02/arrow-function.js
+++ basics_02/arrow-function.js
@@ -1,6 +1,6 @@
-// // const newLocal = function (birthyear) {
-// //     return 2024 - birthyear;
-// // };
+// const newLocal = function (birthyear) {
+//     return 2024 - birthyear;
+// };
 
 // const calcage2 = birthyear=> 2024-birthyear;
 // const age2 =calcage2(2007);
@@ -18,3 +18,24 @@ console.log(yearsuntilretirment(1980,"abhishek"))
 console.log(yearsuntilretirment(2001,"john"))
 console.log(yearsuntilretirment(2004,"killer bean"))
 console.log(yearsuntilretirment(2000," mr issac"))
+
+// function calling other functions
+
+function cutfruits (fruit ,pop){
+
+    return fruit * 4;
+    return pop *4;
+}
+
+function fruitprocessor(apples , oranges ,anar){
+
+   const applejuice  = cutfruits(apples);
+   const orangejuice = cutfruits(oranges);
+   const anar1 = cutfruits(anar);
+
+   const juice = `juice with ${apples} apples and ${oranges} oranges  and ${anar} anar. ${applejuice} pieces of apple ${orangejuice} pieces of orange and ${anar1} ś   `
+   return juice;
+}
+ 
+const output =  fruitprocessor(2,5,3);
+console.log(output)
