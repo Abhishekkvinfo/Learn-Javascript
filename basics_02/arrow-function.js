0e485310f868e6100137944de0e061f626167c00
Abhishekkvinfo <abishekkvinfo@gmail.com>
2024-08-16 19:38:41 +0530

    arrow functions

--- basics_02/arrow-function.js
+++ basics_02/arrow-function.js
@@ -7,10 +7,14 @@
 // console.log(age2);
 
 
-const yearsuntilretirment = birthyear =>{
+const yearsuntilretirment =( birthyear , firstname) =>{
 
     const age = 2024 - birthyear;
     const retirment = 65 - age;
-    return retirment;
+    // return retirment;
+    return ` ${firstname} will retires after ${retirment} years`
 }
-console.log(yearsuntilretirment(2007))
+console.log(yearsuntilretirment(1980,"abhishek"))
+console.log(yearsuntilretirment(2001,"john"))
+console.log(yearsuntilretirment(2004,"killer bean"))
+console.log(yearsuntilretirment(2000," mr issac"))
