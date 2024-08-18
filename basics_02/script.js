c9dc09e063641f2db2232d77d4cbfd77cf1cc339
Abhishekkvinfo <abishekkvinfo@gmail.com>
2024-08-18 16:03:35 +0530

    script

--- basics_02/script.js
+++ basics_02/script.js
@@ -8,13 +8,17 @@ const yearsuntilretirement1 = function (birthyear1, name) {
 
     const age1 = calcage1(birthyear1);
     const retirement1 = 65 - age1;
-    if (retirement1 < 0) {
+    if (retirement1 <=0) {
+
         console.log("already been retired");
+        return retirement1;
     }
     else {
 
+    
         return `${name} will retires after ${retirement1} years`
     }
 }
-const output = yearsuntilretirement1(1987 ,"Abhishek ")
-console.log(output)
+
+const output3 =yearsuntilretirement1 (1959, "abhishek")
+console.log(output3)
