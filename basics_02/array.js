d93176a5a5f107773cf26858e10cdcbd6fd793ab
Abhishekkvinfo <abishekkvinfo@gmail.com>
2024-08-19 21:54:51 +0530

    array

--- basics_02/array.js
+++ basics_02/array.js
@@ -7,3 +7,11 @@ console.log(friends);
 const persons = new Array('Abhishek','Arjun','Ashok','salini','neha','Irfan','sanjay')
 
 console.log(persons)
+
+const years = [1,3,2,4,6,7,7,6,35,43,34,45,53,55,35,65,656,3,35553,5,35,535,553,5,3,25,6,34,464]
+
+console.log(years)
+
+const year =new Array (1,3,2,4,6,7,7,6,35,43,34,45,53,55,35,65,656,3,35553,5,35,535,553,5,3,25,6,34,464)
+
+console.log(year[1])
