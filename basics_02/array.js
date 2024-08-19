00213d1ea415fa7e8a131dc9c5e77bd67a8c2781
Abhishekkvinfo <abishekkvinfo@gmail.com>
2024-08-19 21:47:15 +0530

    array

---
+++ basics_02/array.js
@@ -0,0 +1,9 @@
+
+
+const friends = ['Abhishek','Arjun','Ashok','salini','neha','Irfan','sanjay']
+
+console.log(friends);
+
+const persons = new Array('Abhishek','Arjun','Ashok','salini','neha','Irfan','sanjay')
+
+console.log(persons)
