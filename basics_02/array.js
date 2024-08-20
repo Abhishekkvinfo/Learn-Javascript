274860c4d270c277cafe8e246dcb240b04bd738b
Abhishekkvinfo <abishekkvinfo@gmail.com>
2024-08-20 22:04:53 +0530

    array

--- basics_02/array.js
+++ basics_02/array.js
@@ -8,10 +8,24 @@ const persons = new Array('Abhishek','Arjun','Ashok','salini','neha','Irfan','sa
 
 console.log(persons)
 
-const years = [1,3,2,4,6,7,7,6,35,43,34,45,53,55,35,65,656,3,35553,5,35,535,553,5,3,25,6,34,464]
+const yearss = [1,3,2,4,6,7,7,6,35,43,34,45,53,55,35,65,656,3,35553,5,35,535,553,5,3,25,6,34,464]
 
-console.log(years)
 
 const year =new Array (1,3,2,4,6,7,7,6,35,43,34,45,53,55,35,65,656,3,35553,5,35,535,553,5,3,25,6,34,464)
 
 console.log(year[1])
+
+friends[2] ='Emliy'
+console.log(friends)
+
+
+const calcage = function(birthyear){
+
+    return 2024 - birthyear;
+}
+
+const years =[2000,2001,2002,2003,2004,2005,2006,2007];
+
+const age1 = calcage([5]);
+
+console.log(age1);
