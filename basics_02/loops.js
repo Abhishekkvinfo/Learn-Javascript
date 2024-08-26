6529202d9e7929188ea3d4a1fe69795a5ed52741
Abhishek <145715948+Abhishekkvinfo@users.noreply.github.com>
2024-08-26 22:51:58 +0530

    loops

--- basics_02/loops.js
+++ basics_02/loops.js
@@ -16,11 +16,23 @@
 //     console.log(person[i],typeof person)
 // }
 
-const years =[1999,2007,2006,2002]
+const years =[1999,2007,2006,2002,2001,1947]
 const ages =[];
 
 for(let i=0; i<years.length;i++){
 
    ages.push( 2024 - years[i])
 }
-console.log(ages)
+console.log(ages);
+
+
+//backward looping 
+
+const details =[
+    "abhishek","smilesh","jonathan","jibair"
+]
+
+for(let i = details.length; i >=0;i--){
+
+    console.log(details[i])
+}
