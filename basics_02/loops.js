1b8aa0571ebeb61adad3fe7978c105368fe2e7c7
Abhishek <145715948+Abhishekkvinfo@users.noreply.github.com>
2024-08-26 20:53:56 +0530

    loops

--- basics_02/loops.js
+++ basics_02/loops.js
@@ -1,17 +1,26 @@
 
-// for(let i=1; i <=10;i++){
+// // for(let i=1; i <=10;i++){
 
-//     document.write(i)
+// //     document.write(i)
+// // }
+
+// let person =[
+//     "abhishek",
+//     "aron",
+//     'smilesh',
+//     'appu',
+//     'smilesh'
+// ]
+
+// for(let i=0; i<person.length;i++ ){
+//     console.log(person[i],typeof person)
 // }
 
-let person =[
-    "abhishek",
-    "aron",
-    'smilesh',
-    'appu',
-    'smilesh'
-]
+const years =[1999,2007,2006,2002]
+const ages =[];
 
-for(let i=0; i<person.length;i++ ){
-    console.log(person[i])
+for(let i=0; i<years.length;i++){
+
+   ages.push( 2024 - years[i])
 }
+console.log(ages)
