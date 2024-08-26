7f3e6f3dbdd794563e47280414fc6a4c66886db4
Abhishek <145715948+Abhishekkvinfo@users.noreply.github.com>
2024-08-26 20:19:39 +0530

    loops

--- basics_02/loops.js
+++ basics_02/loops.js
@@ -1,5 +1,16 @@
 
-for(let i=1; i <=10;i++){
+// for(let i=1; i <=10;i++){
 
-    document.write(i)
+//     document.write(i)
+// }
+
+const person = [
+    "Abhishek",
+    "software developer",
+    "Age 18 years old"
+]
+
+for(let i=0;i<=4;i++){
+
+    document.write(person)[i]
 }
