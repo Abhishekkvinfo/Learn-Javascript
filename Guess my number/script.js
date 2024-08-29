6cea28c35e28dd0bb2c433d73a91feb801cf9b5c
Abhishek <145715948+Abhishekkvinfo@users.noreply.github.com>
2024-08-29 21:39:11 +0530

    added-newfeature

--- Guess my number/script.js
+++ Guess my number/script.js
@@ -4,11 +4,18 @@ const random = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 document.querySelector(".check").addEventListener("click", function () {
   const display = Number(document.querySelector(".display").value);
+  // when player wins
   if (display === random) {
     document.querySelector(".correct-number").textContent =
       "🎉 Congratulations YOU WON!! ";
+    document.querySelector("body").style.backgroundColor = "green";
+    document.querySelector(".display").style.backgroundColor = "green";
+    document.querySelector(".output").style.width = "20rem";
+    // when there is no input
   } else if (display === 0) {
     document.querySelector(".correct-number").textContent = "⛔️ no number";
+
+    // when its high
   } else if (display > random) {
     if (score > 0) {
       document.querySelector(".correct-number").textContent = " 📉 Too high";
@@ -18,6 +25,8 @@ document.querySelector(".check").addEventListener("click", function () {
       document.querySelector(".correct-number").textContent =
         "😑😑 YOU LOST THE GAME";
     }
+
+    //when its too low
   } else if (display < random) {
     if (score > 0) {
       document.querySelector(".correct-number").textContent = " 📈 Too low";
