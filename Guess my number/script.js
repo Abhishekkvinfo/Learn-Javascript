8713efe81936a89e1cd9935012d6c04222f4d7a3
Abhishek <145715948+Abhishekkvinfo@users.noreply.github.com>
2024-08-29 19:09:21 +0530

    problem solving

--- Guess my number/script.js
+++ Guess my number/script.js
@@ -1,16 +1,24 @@
 "use strict";
 
 const random = Math.trunc(Math.random() * 20) + 1;
-
+let score = 20;
 document.querySelector(".check").addEventListener("click", function () {
   const display = Number(document.querySelector(".display").value);
   if (display === random) {
     document.querySelector(".correct-number").textContent =
       "🎉 Congratulations YOU WON!! ";
+  } else if (display === 0) {
+    document.querySelector(".correct-number").textContent = "⛔️ no number";
   } else if (display > random) {
     document.querySelector(".correct-number").textContent = " 📉 Too high";
+    score--;
+    document.querySelector(".score").textContent = `💯Score :${score}`;
+    document.querySelector(".correct-number").textContent =
+      "😑 YOU LOST THE GAME";
   } else if (display < random) {
     document.querySelector(".correct-number").textContent = " 📈 Too low";
+    score--;
+    document.querySelector(".score").textContent = `💯Score :${score}`;
   }
 });
 document.querySelector(".output").textContent = `${random}`;
