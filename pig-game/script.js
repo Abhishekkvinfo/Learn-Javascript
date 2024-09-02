bcb7771576f3d94a0e119a8b2754bd696d3fdac1
Abhishek <145715948+Abhishekkvinfo@users.noreply.github.com>
2024-09-02 19:57:35 +0530

    new-feature-added

--- pig-game/script.js
+++ pig-game/script.js
@@ -1,1 +1,10 @@
 "use strict";
+
+const score0 = document.querySelector("#score1");
+const score1 = document.querySelector("#score2");
+const dice = document.querySelector(".dice");
+
+score0.textContent = "0";
+score1.textContent = "0";
+
+dice.classList.remove("dice");
