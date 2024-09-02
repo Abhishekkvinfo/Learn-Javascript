a2353569a70141de001918925ee2c23f4b746e6f
Abhishek <145715948+Abhishekkvinfo@users.noreply.github.com>
2024-09-02 20:05:59 +0530

    selected-all-elements-by-using-qureySelector

--- pig-game/script.js
+++ pig-game/script.js
@@ -3,6 +3,9 @@
 const score0 = document.querySelector("#score1");
 const score1 = document.querySelector("#score2");
 const dice = document.querySelector(".dice");
+const btnNew = document.querySelector(".newgame");
+const btnRoll = document.querySelector(".roll");
+const btnHOLD = document.querySelector(".hold");
 
 score0.textContent = "0";
 score1.textContent = "0";
