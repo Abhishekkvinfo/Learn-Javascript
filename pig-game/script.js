dce54776a9199e78dba0abcbff60befcf0ea59f9
Abhishek <145715948+Abhishekkvinfo@users.noreply.github.com>
2024-09-03 16:25:20 +0530

    development-completed

--- pig-game/script.js
+++ pig-game/script.js
@@ -1,18 +1,18 @@
 "use strict";
 
 //selecting elements
-const score0 = document.querySelector("#score1");
-const score1 = document.querySelector("#score2");
+const score0 = document.querySelector("#score-0");
+const score1 = document.querySelector("#score-1");
 const dice1 = document.querySelector(".dice");
 const btnNew = document.querySelector(".newgame");
 const btnRoll = document.querySelector(".roll");
 const btnHOLD = document.querySelector(".hold");
-const currentscore0 = document.querySelector("#currentscore-0");
-const currentscore1 = document.querySelector("#currentscore-1");
+let currentscore0 = document.querySelector("#currentscore-0");
+let currentscore1 = document.querySelector("#currentscore-1");
 const player1storedscore = document.querySelector(".score");
 let currentscore = 0;
 let activeplayer = 0;
-const scores = [0, 0];
+let scores = [0, 0];
 //starting conditions
 
 score0.textContent = "0";
@@ -35,4 +35,21 @@ btnRoll.addEventListener("click", function () {
     document.getElementsByClassid(`currentscore-${activeplayer}`).textContent =
       currentscore;
   }
+});
+
+btnHOLD.addEventListener("click", function () {
+  scores[activeplayer] += currentscore;
+  document.getElementById(`score-${activeplayer}`).textContent =
+    scores[activeplayer];
+  currentscore = 0;
+});
+
+btnNew.addEventListener("click", function () {
+  let currentscore = 0;
+  let activeplayer = 0;
+  let scores = [0, 0];
+  score0.textContent = "0";
+  score1.textContent = "0";
+  document.querySelector("#currentscore-0").textContent = "0";
+  document.querySelector("#currentscore-1").textContent = "0";
 });
