512c7242548da9d04efa5731ae97eb5336a25dff
Abhishek <145715948+Abhishekkvinfo@users.noreply.github.com>
2024-09-05 23:11:21 +0530

    dsa

--- data-structure/script.js
+++ data-structure/script.js
@@ -1,5 +1,17 @@
 "use strict";
 
-const resturent = {
-  name: "chichnok",
+const restaurant = {
+  name: "classico italiano",
+  location: "kaladi 7th street 23",
+  categories: ["italian", "indian", "american"],
+  startermenu: ["sharjah shake", "light lime", "garlic bread"],
+  mainmenu: ["pizza", "pasta", "risotto"],
 };
+
+const arr = [2, 6, 8];
+
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+
+console.log(a, b, c);
--- test/test.js
+++ test/test.js
@@ -7,3 +7,9 @@ const restaurant = {
   startermenu: ["sharjah shake", "light lime", "garlic bread"],
   mainmenu: ["pizza", "pasta", "risotto"],
 };
+
+const arr = [2, 6, 8];
+
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
