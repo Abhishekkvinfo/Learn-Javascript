e5c1b7a0f1236899f639c540c881b8dccc205cbe
Abhishek <145715948+Abhishekkvinfo@users.noreply.github.com>
2024-09-29 23:11:58 +0530

    split method

--- test/sample.js
+++ test/sample.js
@@ -161,80 +161,84 @@
 // working with strings
 // fundamentals of working with strings
 
-const airline = "AIR INDIA ";
-const airoplane = "A767";
+// const airline = "AIR INDIA ";
+// const airoplane = "A767";
 
-console.log(airline[0]);
-console.log(airline[1]);
-console.log(airline[2]);
-console.log(airline[3]);
-console.log(airline[4]);
-console.log(airline[5]);
-console.log(airline[6]);
-console.log(airline[7]);
-console.log(airline[8]);
+// console.log(airline[0]);
+// console.log(airline[1]);
+// console.log(airline[2]);
+// console.log(airline[3]);
+// console.log(airline[4]);
+// console.log(airline[5]);
+// console.log(airline[6]);
+// console.log(airline[7]);
+// console.log(airline[8]);
 
-// index method
+// // index method
 
-console.log(airline.indexOf("A"));
-console.log(airline.lastIndexOf("A"));
-console.log(airline.slice(4, 7));
+// console.log(airline.indexOf("A"));
+// console.log(airline.lastIndexOf("A"));
+// console.log(airline.slice(4, 7));
 
-//slice method
+// //slice method
 
-const middleseat = function (seat) {
-  const s = seat.slice(-1);
-  if (s === "A" || s === "C") {
-    console.log(`you got middle seat`);
-  } else console.log("you are lucky");
-};
+// const middleseat = function (seat) {
+//   const s = seat.slice(-1);
+//   if (s === "A" || s === "C") {
+//     console.log(`you got middle seat`);
+//   } else console.log("you are lucky");
+// };
 
-middleseat("11A");
-middleseat("B1B");
-middleseat("C1C");
-// upper case lower case method
-const personname = "jOnas";
-console.log(personname);
-const tolowercase = personname.toLowerCase();
-const touppercase = tolowercase[0].toUpperCase() + tolowercase.slice(1);
-console.log(touppercase);
+// middleseat("11A");
+// middleseat("B1B");
+// middleseat("C1C");
+// // upper case lower case method
+// const personname = "jOnas";
+// console.log(personname);
+// const tolowercase = personname.toLowerCase();
+// const touppercase = tolowercase[0].toUpperCase() + tolowercase.slice(1);
+// console.log(touppercase);
 
-const email = "abishekkvinfo@gmail.com";
-const compareemail = "  abishekkvinfo@gmail.com ";
+// const email = "abishekkvinfo@gmail.com";
+// const compareemail = "  abishekkvinfo@gmail.com ";
 
-const crtemail = compareemail.toLowerCase().trim();
-console.log(crtemail);
-if (email === crtemail) {
-  console.log("This is a valid email ");
-}
+// const crtemail = compareemail.toLowerCase().trim();
+// console.log(crtemail);
+// if (email === crtemail) {
+//   console.log("This is a valid email ");
+// }
 
-//replace method
+// //replace method
 
-const indian = "282.98₹";
-const euro = indian.replace(".", ",").replace("₹", "€");
-console.log(euro);
+// const indian = "282.98₹";
+// const euro = indian.replace(".", ",").replace("₹", "€");
+// console.log(euro);
 
-const something = "are you alone alone ??";
-const newsomething = something.replace(/alone/g, "lonely");
-console.log(newsomething);
+// const something = "are you alone alone ??";
+// const newsomething = something.replace(/alone/g, "lonely");
+// console.log(newsomething);
 
-// includes method
+// // includes method
 
-const airport = function (items) {
-  const package = items.toLowerCase();
-  if (
-    package.includes("gun") ||
-    package.includes("knife") ||
-    package.includes("gold")
-  ) {
-    console.log("you are not allowed to enter the country");
-  } else {
-    console.log("welcome to INDA!!");
-  }
-};
-airport("i have a gun with two bullets ");
-airport("i have laptop and smartphone ");
-airport("i have  knife and apples ");
-airport("i have a bag with two bullets ");
-airport("i have gold");
-airport("i have candys ");
+// const airport = function (items) {
+//   const package = items.toLowerCase();
+//   if (
+//     package.includes("gun") ||
+//     package.includes("knife") ||
+//     package.includes("gold")
+//   ) {
+//     console.log("you are not allowed to enter the country");
+//   } else {
+//     console.log("welcome to INDA!!");
+//   }
+// // };
+// airport("i have a gun with two bullets ");
+// airport("i have laptop and smartphone ");
+// airport("i have  knife and apples ");
+// airport("i have a bag with two bullets ");
+// airport("i have gold");
+// airport("i have candys ");
+
+// split method
+
+console.log("ABHI+SHEK".split("+"));
