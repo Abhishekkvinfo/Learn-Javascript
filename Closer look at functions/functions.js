6219735a08c2c1ae0d1c4e10c946f0f39a602308
Abhishek <145715948+Abhishekkvinfo@users.noreply.github.com>
2024-10-01 20:43:32 +0530

    a closer look at functions

--- Closer look at functions/functions.js
+++ Closer look at functions/functions.js
@@ -1,3 +1,16 @@
 "strict";
+// a closer look at functions
+const bookings = [];
+const plane = function (flightname, noofpassengers = 1, price = 900) {
+  const booking = {
+    flightname,
+    noofpassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
 
-const plane = function (flightname, noofpassengers = 1, price = 900`$`) {};
+plane("guhan");
+plane("guhan");
+plane("guhan");
