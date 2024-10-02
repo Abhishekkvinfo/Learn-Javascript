6fc33d10ea18c5c83f44939be792f0a8ec3def0f
Abhishek <145715948+Abhishekkvinfo@users.noreply.github.com>
2024-10-02 20:54:24 +0530

    passing arguments

--- Closer look at functions/functions.js
+++ Closer look at functions/functions.js
@@ -1,16 +1,35 @@
-"strict";
-// a closer look at functions
-const bookings = [];
-const plane = function (flightname, noofpassengers = 1, price = 900) {
-  const booking = {
-    flightname,
-    noofpassengers,
-    price,
-  };
-  console.log(booking);
-  bookings.push(booking);
+// "strict";
+// // a closer look at functions
+// const bookings = [];
+// const plane = function (flightname, noofpassengers = 1, price = 900) {
+//   const booking = {
+//     flightname,
+//     noofpassengers,
+//     price,
+//   };
+//   console.log(booking);
+//   bookings.push(booking);
+// };
+
+// plane("john");
+// plane("abhshek");
+// plane("donald");
+
+const flight = "LH234";
+const abhishek = {
+  name: "abhishek",
+  passport: 12343456,
 };
 
-plane("guhan");
-plane("guhan");
-plane("guhan");
+const checkin = function (flightnum, passenger) {
+  flightnum = "LH999";
+  passenger = abhishek.name = "Mr " + abhishek.name;
+  if (abhishek.passport === 12343456) {
+    alert("CHECKED IN");
+  } else {
+    alert("Wrong passport");
+  }
+};
+checkin(flight, abhishek);
+console.log(flight);
+console.log(abhishek);
