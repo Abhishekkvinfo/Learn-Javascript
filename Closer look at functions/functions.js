75b84d19d3d9bfa91cf3641604b32bc209b34990
Abhishek <145715948+Abhishekkvinfo@users.noreply.github.com>
2024-10-05 19:54:16 +0530

    function returning functions

--- Closer look at functions/functions.js
+++ Closer look at functions/functions.js
@@ -1,68 +1,70 @@
-// "strict";
-// // a closer look at functions
-// const bookings = [];
-// const plane = function (flightname, noofpassengers = 1, price = 900) {
-//   const booking = {
-//     flightname,
-//     noofpassengers,
-//     price,
+// // "strict";
+// // // a closer look at functions
+// // const bookings = [];
+// // const plane = function (flightname, noofpassengers = 1, price = 900) {
+// //   const booking = {
+// //     flightname,
+// //     noofpassengers,
+// //     price,
+// //   };
+// //   console.log(booking);
+// //   bookings.push(booking);
+// // };
+
+// // plane("john");
+// // plane("abhshek");
+// // plane("donald");
+
+// // const abhishek = {
+// //   name: "abhishek kv",
+// //   details: {
+// //     flight: "LH888",
+// //     passport: 789456123,
+// //   },
+// // };
+
+// // const storedetails = [];
+
+// // const flightnum = function (flightnum, passengername, passport) {
+// //   abhishek.name = "MR" + abhishek.name.toUpperCase();
+// //   if (passport === abhishek.details.passport) {
+// //     alert("you have a standard 😂");
+// //   } else {
+// //     alert("you are a fraud");
+// //   }
+// // };
+// // flightnum("swqnjk", "qwed", 789456123);
+// // flightnum("nothing", "already defined", 789456123);
+
+// // high order functions
+
+// const oneword = function (str) {
+//   return str;
+// };
+
+// const transformer = function (str, star, fn) {
+//   console.log(`transformed string:${fn(star)}`);
+// };
+
+// transformer("javascript is best", "dont mess with me", oneword);
+
+// // const high5 = function () {
+// //   console.log("hello");
+// // };
+
+// // document.body.addEventListener("click", high5);
+
+// // function returning functions
+
+// const greet = function (greetings) {
+//   return function (name) {
+//     console.log(`${greetings},${name} `);
 //   };
-//   console.log(booking);
-//   bookings.push(booking);
 // };
+// const greetingsde = greet("hey");
 
-// plane("john");
-// plane("abhshek");
-// plane("donald");
+// greetingsde("ABHISHERK");
 
-// const abhishek = {
-//   name: "abhishek kv",
-//   details: {
-//     flight: "LH888",
-//     passport: 789456123,
-//   },
-// };
+// const greetarr = (greeting) => (name) => console.log(`${greeting} ${name}`);
 
-// const storedetails = [];
-
-// const flightnum = function (flightnum, passengername, passport) {
-//   abhishek.name = "MR" + abhishek.name.toUpperCase();
-//   if (passport === abhishek.details.passport) {
-//     alert("you have a standard 😂");
-//   } else {
-//     alert("you are a fraud");
-//   }
-// };
-// flightnum("swqnjk", "qwed", 789456123);
-// flightnum("nothing", "already defined", 789456123);
-
-// high order functions
-
-const oneword = function (str) {
-  return str;
-};
-
-const transformer = function (str, star, fn) {
-  console.log(`transformed string:${fn(star)}`);
-};
-
-transformer("javascript is best", "dont mess with me", oneword);
-
-// const high5 = function () {
-//   console.log("hello");
-// };
-
-// document.body.addEventListener("click", high5);
-
-const greet = function (greetings) {
-  return function (name) {
-    console.log(`${greetings},${name} `);
-  };
-};
-const greetingsde = greet("hey");
-
-greetingsde("ABHISHERK");
-
-const greetarr = (greeting) => (name) => console.log(`${greeting} ${name}`);
-
-greetarr("hi")("Abhishek");
+// greetarr("hi")("Abhishek");
