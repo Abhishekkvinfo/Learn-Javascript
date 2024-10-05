46b3181fb0c427cc374315d184a9ae288bad700f
Abhishek <145715948+Abhishekkvinfo@users.noreply.github.com>
2024-10-05 14:41:34 +0530

    high order functions

--- Closer look at functions/functions.js
+++ Closer look at functions/functions.js
@@ -15,23 +15,35 @@
 // plane("abhshek");
 // plane("donald");
 
-const abhishek = {
-  name: "abhishek kv",
-  details: {
-    flight: "LH888",
-    passport: 789456123,
-  },
+// const abhishek = {
+//   name: "abhishek kv",
+//   details: {
+//     flight: "LH888",
+//     passport: 789456123,
+//   },
+// };
+
+// const storedetails = [];
+
+// const flightnum = function (flightnum, passengername, passport) {
+//   abhishek.name = "MR" + abhishek.name.toUpperCase();
+//   if (passport === abhishek.details.passport) {
+//     alert("you have a standard 😂");
+//   } else {
+//     alert("you are a fraud");
+//   }
+// };
+// flightnum("swqnjk", "qwed", 789456123);
+// flightnum("nothing", "already defined", 789456123);
+
+// high order functions
+
+const oneword = function (str) {
+  return str;
 };
 
-const storedetails = [];
-
-const flightnum = function (flightnum, passengername, passport) {
-  abhishek.name = "MR" + abhishek.name.toUpperCase();
-  if (passport === abhishek.details.passport) {
-    alert("you have a standard 😂");
-  } else {
-    alert("you are a fraud");
-  }
+const transformer = function (str, star, fn) {
+  console.log(`transformed string:${fn(star)}`);
 };
-flightnum("swqnjk", "qwed", 789456123);
-flightnum("nothing", "already defined", 789456123);
+
+transformer("javascript is best", "dont mess with me", oneword);
