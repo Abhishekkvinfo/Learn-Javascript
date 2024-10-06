22601d6079b95e1e4f704b866e3220c5009835e4
Abhishek <145715948+Abhishekkvinfo@users.noreply.github.com>
2024-10-06 22:25:53 +0530

    poll application

--- Closer look at functions/functions.js
+++ Closer look at functions/functions.js
@@ -19,108 +19,126 @@
 //   name: "abhishek kv",
 //   details: {
 //     flight: "LH888",
-//     passport: 789456123,
+// //     passport: 789456123,
+// //   },
+// // };
+
+// // const storedetails = [];
+
+// // const flightnum = function (flightnum, passengername, passport) {
+// //   abhishek.name = "MR" + abhishek.name.toUpperCase();
+// //   if (passport === abhishek.details.passport) {
+// //     alert("you have a standard 😂");
+// //   } else {
+// //     alert("you are a fraud");
+// //   }
+// // };
+// // flightnum("swqnjk", "qwed", 789456123);
+// // flightnum("nothing", "already defined", 789456123);
+
+// // high order functions
+
+// // const oneword = function (str) {
+// //   return str;
+// // };
+
+// // const transformer = function (str, star, fn) {
+// //   console.log(`transformed string:${fn(star)}`);
+// // };
+
+// // transformer("javascript is best", "dont mess with me", oneword);
+
+// // // const high5 = function () {
+// // //   console.log("hello");
+// // // };
+
+// // document.body.addEventListener("click", high5);
+
+// // function returning functions
+
+// // const greet = function (greetings) {
+// //   return function (name) {
+// //     console.log(`${greetings},${name} `);
+// //   };
+// // };
+// // const greetingsde = greet("hey");
+
+// // greetingsde("ABHISHERK");
+
+// // const greetarr = (greeting) => (name) => console.log(`${greeting} ${name}`);
+
+// // greetarr("hi")("Abhishek");
+
+// // the call and apply methods
+
+// const airindia = {
+//   airline: "Air india",
+//   iatacode: "LH",
+//   bookings: [],
+
+//   book(flightnum, name) {
+//     console.log(
+//       `${name} booked a seat on ${this.airline} flight ${this.iatacode}${flightnum}`
+//     );
+//     this.bookings.push({ flight: `${this.iatacode}${flightnum}`, name });
 //   },
 // };
 
-// const storedetails = [];
+// airindia.book(233, "Abhishek kv");
+// // airindia.book(233, " shibu v");
+// // airindia.book(233, "sambu pv");
+// // airindia.book(233, "irfan kv");
+// // airindia.book(233, "abu");
 
-// const flightnum = function (flightnum, passengername, passport) {
-//   abhishek.name = "MR" + abhishek.name.toUpperCase();
-//   if (passport === abhishek.details.passport) {
-//     alert("you have a standard 😂");
-//   } else {
-//     alert("you are a fraud");
-//   }
-// };
-// flightnum("swqnjk", "qwed", 789456123);
-// flightnum("nothing", "already defined", 789456123);
+// // console.log(airindia.bookings);
 
-// high order functions
+// // apply call  methods
 
-// const oneword = function (str) {
-//   return str;
+// const indiandream = {
+//   airline: "indiandreams",
+//   name: "",
+//   iatacode: "XX",
+//   bookings: [],
 // };
 
-// const transformer = function (str, star, fn) {
-//   console.log(`transformed string:${fn(star)}`);
+// // const indiandreams = airindia.book;
+
+// // indiandreams.call(indiandream, 222, "Hima");
+
+// const qatarairways = airindia.book;
+
+// const qatarairway = {
+//   airline: "Qatar Airways",
+//   name: "",
+//   iatacode: "AA",
+//   bookings: [],
 // };
 
-// transformer("javascript is best", "dont mess with me", oneword);
+// // qatarairways.call(qatarairway, 897, "Abdul");
 
-// // const high5 = function () {
-// //   console.log("hello");
-// // };
+// // bind method
 
-// document.body.addEventListener("click", high5);
+// const qatar = airindia.book.bind(qatarairway, 23);
 
-// function returning functions
+// qatar("JONATHAN JAMES");
+// qatar("JAMES");
+// qatar("martha cooper");
+// qatar("sony ");
 
-// const greet = function (greetings) {
-//   return function (name) {
-//     console.log(`${greetings},${name} `);
-//   };
-// };
-// const greetingsde = greet("hey");
+//  poll application
 
-// greetingsde("ABHISHERK");
+const poll = {
+  question: "what is your favourite programing language ?",
+  options: ["0:Javascript", "1:python", "2:Rust", "3:C++"],
+  answers: new Array(4).fill(0),
 
-// const greetarr = (greeting) => (name) => console.log(`${greeting} ${name}`);
-
-// greetarr("hi")("Abhishek");
-
-// the call and apply methods
-
-const airindia = {
-  airline: "Air india",
-  iatacode: "LH",
-  bookings: [],
-
-  book(flightnum, name) {
-    console.log(
-      `${name} booked a seat on ${this.airline} flight ${this.iatacode}${flightnum}`
+  registernewarry() {
+    const answer = prompt(
+      `${this.question}\n ${this.options.join("\n")}\n(write option number)`
     );
-    this.bookings.push({ flight: `${this.iatacode}${flightnum}`, name });
+    console.log(answer);
   },
 };
-
-airindia.book(233, "Abhishek kv");
-// airindia.book(233, " shibu v");
-// airindia.book(233, "sambu pv");
-// airindia.book(233, "irfan kv");
-// airindia.book(233, "abu");
-
-// console.log(airindia.bookings);
-
-// apply call  methods
-
-const indiandream = {
-  airline: "indiandreams",
-  name: "",
-  iatacode: "XX",
-  bookings: [],
-};
-
-// const indiandreams = airindia.book;
-
-// indiandreams.call(indiandream, 222, "Hima");
-
-const qatarairways = airindia.book;
-
-const qatarairway = {
-  airline: "Qatar Airways",
-  name: "",
-  iatacode: "AA",
-  bookings: [],
-};
-
-// qatarairways.call(qatarairway, 897, "Abdul");
-
-// bind method
-
-const qatar = airindia.book.bind(qatarairway, 23);
-
-qatar("JONATHAN JAMES");
-qatar("JAMES");
-qatar("martha cooper");
-qatar("sony ");
+document
+  .querySelector(".poll")
+  .addEventListener("click", poll.registernewarry.bind(poll));
