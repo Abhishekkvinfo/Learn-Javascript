878d302780f4fd3266efe992c2ebaf0f3ded3cbc
Abhishek <145715948+Abhishekkvinfo@users.noreply.github.com>
2024-10-06 18:20:21 +0530

    bind method

--- Closer look at functions/functions.js
+++ Closer look at functions/functions.js
@@ -1,60 +1,60 @@
-// // "strict";
-// // // a closer look at functions
-// // const bookings = [];
-// // const plane = function (flightname, noofpassengers = 1, price = 900) {
-// //   const booking = {
-// //     flightname,
-// //     noofpassengers,
-// //     price,
-// //   };
-// //   console.log(booking);
-// //   bookings.push(booking);
-// // };
-
-// // plane("john");
-// // plane("abhshek");
-// // plane("donald");
-
-// // const abhishek = {
-// //   name: "abhishek kv",
-// //   details: {
-// //     flight: "LH888",
-// //     passport: 789456123,
-// //   },
-// // };
-
-// // const storedetails = [];
-
-// // const flightnum = function (flightnum, passengername, passport) {
-// //   abhishek.name = "MR" + abhishek.name.toUpperCase();
-// //   if (passport === abhishek.details.passport) {
-// //     alert("you have a standard 😂");
-// //   } else {
-// //     alert("you are a fraud");
-// //   }
-// // };
-// // flightnum("swqnjk", "qwed", 789456123);
-// // flightnum("nothing", "already defined", 789456123);
-
-// // high order functions
-
-// const oneword = function (str) {
-//   return str;
+// "strict";
+// // a closer look at functions
+// const bookings = [];
+// const plane = function (flightname, noofpassengers = 1, price = 900) {
+//   const booking = {
+//     flightname,
+//     noofpassengers,
+//     price,
+//   };
+//   console.log(booking);
+//   bookings.push(booking);
 // };
 
-// const transformer = function (str, star, fn) {
-//   console.log(`transformed string:${fn(star)}`);
+// plane("john");
+// plane("abhshek");
+// plane("donald");
+
+// const abhishek = {
+//   name: "abhishek kv",
+//   details: {
+//     flight: "LH888",
+//     passport: 789456123,
+//   },
 // };
 
-// transformer("javascript is best", "dont mess with me", oneword);
+// const storedetails = [];
 
-// // const high5 = function () {
-// //   console.log("hello");
-// // };
+// const flightnum = function (flightnum, passengername, passport) {
+//   abhishek.name = "MR" + abhishek.name.toUpperCase();
+//   if (passport === abhishek.details.passport) {
+//     alert("you have a standard 😂");
+//   } else {
+//     alert("you are a fraud");
+//   }
+// };
+// flightnum("swqnjk", "qwed", 789456123);
+// flightnum("nothing", "already defined", 789456123);
 
-// // document.body.addEventListener("click", high5);
+// high order functions
 
-// // function returning functions
+const oneword = function (str) {
+  return str;
+};
+
+const transformer = function (str, star, fn) {
+  console.log(`transformed string:${fn(star)}`);
+};
+
+transformer("javascript is best", "dont mess with me", oneword);
+
+// const high5 = function () {
+//   console.log("hello");
+// };
+
+// document.body.addEventListener("click", high5);
+
+// function returning functions
 
 // const greet = function (greetings) {
 //   return function (name) {
@@ -92,7 +92,7 @@ airindia.book(233, "Abhishek kv");
 
 console.log(airindia.bookings);
 
-// apply call methods
+// apply call  methods
 
 const indiandream = {
   airline: "indiandreams",
@@ -114,4 +114,10 @@ const qatarairway = {
   bookings: [],
 };
 
-indiandreams.call(qatarairway, 897, "Abdul");
+qatarairways.call(qatarairway, 897, "Abdul");
+
+//bind method
+
+const qatar = airindia.book.bind(qatarairway);
+
+qatar(22, "JONATHAN JAMES");
